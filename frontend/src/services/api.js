84df7526ab862e5e45579b944af8cfe3ec0d0540
snehal1805-dev <snehalmatole8@gmail.com>
2026-08-03 23:46:00 +0530

    fix frontend api while hosting

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "memoraextension-production.up.railway.app",
+  baseURL: "https://memoraextension-production.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
